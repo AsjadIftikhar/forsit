a6e2be5997a8435f4a73cc22217ee54433afe28e
Asjad iftikhar <iftikharasjad@gmail.com>
2023-10-04 12:17:17 +0500

    Added Seed File

---
+++ app/database/seed.py
@@ -0,0 +1,88 @@
+import random
+from faker import Faker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database.models import Category, Product, Sale, InventoryTracking
+from datetime import datetime, timedelta
+
+# Create a Faker instance for generating random data
+fake = Faker()
+
+# Define your database connection URL
+database_url = "postgresql://postgres:54321@localhost/forsit_db"
+
+# Create a SQLAlchemy engine and session
+engine = create_engine(database_url)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# Function to generate random datetime within a range
+def random_datetime(start_date, end_date):
+    return start_date + timedelta(
+        seconds=random.randint(0, int((end_date - start_date).total_seconds()))
+    )
+
+
+# Function to create random data for categories
+def create_random_category():
+    category = Category(name=fake.unique.word())
+    return category
+
+
+# Function to create random data for products
+def create_random_product(category):
+    product = Product(
+        name=fake.unique.word(),
+        description=fake.sentence(),
+        price=random.uniform(1.0, 100.0),
+        margin=random.uniform(0.1, 0.5),
+        stock=random.randint(0, 100),
+        category=category,
+    )
+    return product
+
+
+# Function to create random data for sales
+def create_random_sale(product):
+    sale = Sale(
+        sale_date=random_datetime(datetime(2022, 1, 1), datetime(2023, 1, 1)),
+        quantity=random.randint(1, 10),
+        total=random.uniform(10.0, 500.0),
+        revenue=random.uniform(1.0, 100.0),
+        product=product,
+    )
+    return sale
+
+
+# Function to create random data for inventory tracking
+def create_random_inventory_tracking(product):
+    inventory_tracking = InventoryTracking(
+        quantity=random.randint(0, 50),
+        timestamp=random_datetime(datetime(2022, 1, 1), datetime(2023, 1, 1)),
+        product=product,
+    )
+    return inventory_tracking
+
+
+# Seed the database with random data
+try:
+    for _ in range(10):  # Create 10 categories
+        category = create_random_category()
+        session.add(category)
+        for _ in range(10):  # Create 10 products per category
+            product = create_random_product(category)
+            session.add(product)
+            for _ in range(5):  # Create 5 sales and inventory tracking entries per product
+                sale = create_random_sale(product)
+                session.add(sale)
+                inventory_tracking = create_random_inventory_tracking(product)
+                session.add(inventory_tracking)
+
+    session.commit()
+    print("Database seeded successfully.")
+except Exception as e:
+    session.rollback()
+    print("Error seeding the database:", str(e))
+finally:
+    session.close()
--- app/selectors/sales.py
+++ app/selectors/sales.py
@@ -1,12 +1,9 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from app.database.models import Sale, Product
 from typing import Optional
-
 from sqlalchemy import func
 from enum import Enum
-
 from datetime import datetime, timedelta
-
 from fastapi import HTTPException
 
 
@@ -35,7 +32,6 @@ def selector_compare_revenue_by_date_range(db: Session, start_date1: datetime, e
     }
 
 
-# Selector for comparing revenue between two categories
 def selector_compare_revenue_by_categories(db: Session, category_id1: int, category_id2: int):
     total_revenue1 = db.query(func.sum(Sale.revenue)).join(Sale.product).filter(
         Product.category_id == category_id1
@@ -86,29 +82,16 @@ def selector_get_sales_by_filters(
         skip: int = 0,
         limit: int = 10
 ):
-    """
-    Retrieve a list of sales based on provided filters with pagination.
+    query = db.query(Sale).options(
+        joinedload(Sale.product).joinedload(Product.category)
+    )
 
-    :param db: Database session
-    :param product_id: ID of the product to filter by (optional)
-    :param category_id: ID of the category to filter by (optional)
-    :param start_date: Start date of the sale date range (optional)
-    :param end_date: End date of the sale date range (optional)
-    :param skip: Number of records to skip (for pagination)
-    :param limit: Maximum number of records to return (for pagination)
-    :return: List of Sale models based on the provided filters
-    """
-    query = db.query(Sale)
-
-    # Apply product_id filtering if provided
     if product_id is not None:
         query = query.filter(Sale.product_id == product_id)
 
-    # Apply category_id filtering if provided
     if category_id is not None:
-        query = query.join(Sale.product).filter(Product.category_id == category_id)
+        query = query.filter(Product.category_id == category_id)
 
-    # Apply date range filtering if start_date and end_date are provided
     if start_date and end_date:
         query = query.filter(Sale.sale_date >= start_date, Sale.sale_date <= end_date)
 
